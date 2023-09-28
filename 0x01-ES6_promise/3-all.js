b82c19e189793f94c39703b86b5a846463a35346
Ojobumiche <bumiche@gmail.com>
2023-09-28 07:02:58 -0700

    3-all.js

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,14 @@
+//return promises, using function handleProfileSignup()
+import { uploadPhoto, createUser } from './utils.js';
+
+async function handleProfileSignup() {
+  try {
+    const [photo, user] = await Promise.all([uploadPhoto(), createUser()]);
+
+    console.log(user.body.firstName, user.body.lastName);
+  } catch (error) {
+    console.error('Signup system offline');
+  }
+}
+
+export default handleProfileSignup;
