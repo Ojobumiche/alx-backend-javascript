e464331522e6a0913143985e0cc21b41f0f44258
Ojobumiche <bumiche@gmail.com>
2023-09-27 21:47:37 -0700

    done

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,9 +1,10 @@
-function getFullResponseFromAPI(success) {
+// Return a promise. The parameter is a boolean. getFullResponseFromAPI(success)
+export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
     if (success) {
       resolve({
         status: 200,
-        body: 'Success'
+        body: 'Success',
       });
     } else {
       reject(new Error('The fake API is not working currently'));
