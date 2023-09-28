ceb99baf4f47ee0635190cd639fc2ad2c857eb48
Ojobumiche <bumiche@gmail.com>
2023-09-27 17:14:35 -0700

    done

--- 0x01-ES6_promise/0-main.js
+++ 0x01-ES6_promise/0-main.js
@@ -1,4 +1,7 @@
 import getResponseFromAPI from "./0-promise.js";
 
+// Call the function or use the Promise from the imported module
 const response = getResponseFromAPI();
+
+// Log whether the result is an instance of a Promise
 console.log(response instanceof Promise);
