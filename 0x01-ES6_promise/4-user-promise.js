cccc2b6852e74bebb6ea13234aa7dadee7752a1d
Ojobumiche <bumiche@gmail.com>
2023-09-29 00:49:15 +0000

    done

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,3 +1,4 @@
+// Using function signUpUser(firstName, lastName) {}; return firstName and lastName.
 export default function signUpUser(firstName, lastName) {
   return Promise.resolve({
     firstName,
